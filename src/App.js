c1c5be7a582102b3ef66f406a200592575574cc8
sreepriya73 <sreepriyaillikkatt@gmail.com>
2024-06-12 22:09:24 +0530

    search

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddQuotes from './component/AddQuotes';
 import DeleteQuotes from './component/DeleteQuotes';
+import SearchQuotes from './component/SearchQuotes';
 
 function App() {
   return (
     <div>
      <AddQuotes/>
      <DeleteQuotes/>
+     <SearchQuotes/>
     </div>
   );
 }
