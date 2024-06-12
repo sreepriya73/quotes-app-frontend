421f567aa18949a1cee315695899c87ca09da9f7
sreepriya73 <sreepriyaillikkatt@gmail.com>
2024-06-12 22:01:29 +0530

    dlt cpltd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddQuotes from './component/AddQuotes';
+import DeleteQuotes from './component/DeleteQuotes';
 
 function App() {
   return (
     <div>
      <AddQuotes/>
+     <DeleteQuotes/>
     </div>
   );
 }
